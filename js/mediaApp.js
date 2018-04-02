eebd9caec60a452e63755b7e29ab3746e5c76a36
qedpi <junruihu@gmail.com>
2018-04-01 19:09:09 -0700

    add new test page for media

--- js/mediaApp.js
+++ js/mediaApp.js
@@ -1,15 +1,17 @@
 new Vue({
-  el: '#media',
+  el: '#app',
   data: {
     companyInfo: '[placeholder company summary]',
     vidHeight: 300,
     vidWidth: 300,
     showMedia: false,
+    readyToRender: false,
   },
   methods: {
   },
   created() {
     this.vidWidth = Math.min(window.innerWidth * .8, 700);
     this.vidHeight = this.vidWidth * .7;
+    this.readyToRender = true;
   }
 });
