035b13dfb5071c275090907d4d7d68331a30b1cf
qedpi <junruihu@gmail.com>
2018-04-11 14:25:48 -0700

    livestream v2

---
+++ livestream/mediaApp.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#media',
+  data: {
+    companyInfo: '[placeholder company summary]',
+    vidHeight: 300,
+    vidWidth: 300,
+    showMedia: false,
+    readyToRender: false,
+    sponsors: ['CryptoParency', 'dekrypt'],
+  },
+  methods: {
+  },
+  created() {
+    this.vidWidth = Math.min(window.innerWidth * .8, 700);
+    this.vidHeight = this.vidWidth * .7;
+    this.readyToRender = true;
+  }
+});
